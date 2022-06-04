674996c2774597d08acd1ee6fc5de94427dfa1a7
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-04 18:57:05 +0200

    rename style.scss to style.css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@ import Header from "../components/Header.svelte";
 
 </script>
 <svelte:head>
-    <link rel="stylesheet" href="../style.scss" />
+    <link rel="stylesheet" href="/style.css" />
 </svelte:head>
 <Header />
 
