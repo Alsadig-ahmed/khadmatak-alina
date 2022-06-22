6a0d1214ed8324afecbcaf40514b8af44ee7d285
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-22 15:13:29 +0200

    add imgs of the team

--- src/routes/supervisor/index.svelte
+++ src/routes/supervisor/index.svelte
@@ -5,12 +5,12 @@
 
 	let img = '/home.jpg';
 	let supervisors = [
-		{ name: 'عبدالعزيز عصام - أدارة', number: 'tel:+249967013268' },
-		{ name: 'السيد - منسق ترحال', number: 'tel:+249912964409' },
-		{ name: 'ابوعبيدة - منسق دليفري', number: 'tel:+249904373619' },
-		{ name: 'النزير - منسق رقسات', number: 'tel:+249911622268' },
-		{ name: 'عمران - منسق فنيين', number: 'tel:+249916130972' },
-		{ name: 'الهادي - منسق ولاية الخرطوم', number: 'tel:+249910761140' }
+		{ name: 'عبدالعزيز عصام - أدارة', number: 'tel:+249967013268', img:'/imgs/team/IMG-20220621-WA0006.jpg' },
+		{ name: 'السيد - منسق ترحال', number: 'tel:+249912964409', img:'/imgs/team/IMG-20220621-WA0015.jpg' },
+		{ name: 'ابوعبيدة - منسق دليفري', number: 'tel:+249904373619', img:'/imgs/team/IMG-20220621-WA0010.jpg' },
+		{ name: 'النزير - منسق رقسات', number: 'tel:+249911622268', img:'/imgs/team/home.jpg' },
+		{ name: 'عمران - منسق فنيين', number: 'tel:+249916130972', img:'/imgs/team/home.jpg' },
+		{ name: 'الهادي - منسق ولاية الخرطوم', number: 'tel:+249910761140', img:'/imgs/team/IMG-20220621-WA0001.jpg' }
 	];
 </script>
 
@@ -18,6 +18,6 @@
 
 <Wrapper>
 	{#each supervisors as supervisor}
-		<Card {img} text={supervisor.name} url={supervisor.number} />
+		<Card img={supervisor.img} text={supervisor.name} url={supervisor.number} />
 	{/each}
 </Wrapper>
