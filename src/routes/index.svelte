c4831eea1d78f02cebe473268135de0fe84e7b82
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-22 22:15:14 +0200

    add whatsapp link to the page and change some color

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,24 +2,43 @@
 	import Servce from '../components/Servce.svelte';
 	import Header from '../components/Header.svelte';
 	import Wrapper from '$lib/Wrapper.svelte';
+	import Whatsappclr from '$lib/svg-icons/Whatsappclr.svelte';
 
 	let Servces = [
-		{ url: 'tirihal', title: 'أطلب ترحال', bg: '' ,icon:'car'},
-		{ url: 'tel:+249967013268', title: 'توصيل جميع الخدمات',bg: '' ,icon:'shopping-cart' },
-		{ url: 'ragsha', title: 'ترحال رقشات',bg: '' ,icon:'automobile' },
-		{ url: 'tel:+249127871027', title: 'اطلب فنين',bg: '' ,icon:'adjust' },
-		{ url: 'realstate', title: 'ايجار وشراء منازل',bg: '' ,icon:'home' },
-		{ url: 'tel:+249967013268', title: ' شراء + صيانة تلفونات',bg: '' ,icon:'phone' },
-		{ url: 'makeup', title: 'اجار زفاف وبدل  + حجز جلسات تجميل',bg: '' ,icon:'magic' },
-		{ url: 'tel:+249919784021', title: 'طلب خبائز و حلويات',bg: '' ,icon:'lemon-o' },
-		{ url: 'tel:+249967013268', title: 'حجز طباخ',bg: '' ,icon:'paste' }
+		{ url: 'tirihal', title: 'أطلب ترحال', bg:'#9bbe1c', icon: 'car' },
+		{ url: 'tel:+249967013268', title: 'توصيل جميع الخدمات', bg: 'green', icon: 'shopping-cart' },
+		{ url: 'ragsha', title: 'ترحال رقشات', bg: '#2db497', icon: 'automobile' },
+		{ url: 'tel:+249127871027', title: 'اطلب فنين', bg: '', icon: 'adjust' },
+		{ url: 'realstate', title: 'ايجار وشراء منازل', bg: '#a36ccf', icon: 'home' },
+		{ url: 'tel:+249967013268', title: ' شراء + صيانة تلفونات', bg: 'pink', icon: 'phone' },
+		{ url: 'makeup', title: 'اجار زفاف وبدل  + حجز جلسات تجميل', bg: '#3bb9ff', icon: 'magic' },
+		{ url: 'tel:+249919784021', title: 'طلب خبائز و حلويات', bg: '#eac117', icon: 'lemon-o' },
+		{ url: 'tel:+249967013268', title: 'حجز طباخ', bg: '#bb1942', icon: 'paste' }
 	];
 </script>
 
 <Header />
 
 <Wrapper>
+	<a href="https://wa.me/249967013268" target="_blank" rel="noopener noreferrer">
+		رأسلنا علي واتساب
+		<Whatsappclr />
+	</a>
 	{#each Servces as s}
 		<Servce url={s.url} title={s.title} bg={s.bg} icon={s.icon} />
 	{/each}
 </Wrapper>
+
+<style lang="scss">
+	a {
+		background-color: red;
+		color: white;
+		padding: 0.2rem;
+		display: inline-block;
+		border-radius: 0.8rem;
+		&:hover {
+			background-color: rgb(255, 136, 0);
+			color: rgb(129, 118, 118);
+		}
+	}
+</style>
