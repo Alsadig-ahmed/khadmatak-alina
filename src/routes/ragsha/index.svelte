7ab0c84f846f614f45b1af3c1c65acaa947d842d
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-22 19:41:47 +0200

    add imgs

--- src/routes/ragsha/index.svelte
+++ src/routes/ragsha/index.svelte
@@ -3,13 +3,14 @@
 	import Card from '../../components/Card.svelte';
 	import Wrapper from '$lib/Wrapper.svelte';
 	let url = 'tel:+249127871021';
-	let img = '/home.jpg';
+	// let img = '/home.jpg';
+
 </script>
 
 <Header title="ترحال رقشات" />
 
 <Wrapper>
-	<Card {img} {url} text="رقشة جانسي" />
-	<Card {img} {url} text="رقشة حمدوووك" />
-	<Card {img} {url} text="رقشة دقو ورا" />
+	<Card img='/imgs/IMG-20220621-WA0008.jpg' {url} text="رقشة جانسي" />
+	<Card img='/imgs/IMG-20220621-WA0012.jpg' {url} text="رقشة حمدوووك" />
+	<Card img='/imgs/IMG-20220621-WA0016.jpg' {url} text="رقشة دقو ورا" />
 </Wrapper>
