131a04d0938cec41b28cca254d9176ba96f0bf11
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-24 00:10:21 +0200

    include a message to the whatsapp link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
 	import App from '$lib/modal/App.svelte';
 
 	let Servces = [
-		{ url: 'tirihal', title: 'أطلب ترحال', bg:'#9bbe1c', icon: 'car' },
+		{ url: 'tirihal', title: 'أطلب ترحال', bg: '#9bbe1c', icon: 'car' },
 		{ url: 'tel:+249967013268', title: 'توصيل جميع الخدمات', bg: 'green', icon: 'shopping-cart' },
 		{ url: 'ragsha', title: 'ترحال رقشات', bg: '#2db497', icon: 'automobile' },
 		{ url: 'tel:+249127871027', title: 'اطلب فنين', bg: '', icon: 'adjust' },
@@ -21,7 +21,11 @@
 <Header />
 
 <Wrapper>
-	<a href="https://wa.me/249967013268" target="_blank" rel="noopener noreferrer">
+	<a
+		href="https://wa.me/249967013268?text=%E2%9C%8B%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%20%D9%88%D8%B9%D9%84%D9%8A%D9%83%D9%85%0A%20%D8%A7%D8%B1%D8%BA%D8%A8%20%D9%81%D9%8A%20%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85%20%D9%86%D8%B8%D8%A7%D9%85%20%D8%AE%D8%AF%D9%85%D8%A7%D8%AA%D9%83%20%D8%B9%D9%84%D9%8A%D9%86%D8%A7"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
 		رأسلنا علي واتساب
 		<Whatsappclr />
 	</a>
