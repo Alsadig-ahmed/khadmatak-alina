eb6b8652dbc19a99f6dcb6d22081b19318b3de36
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-24 00:09:25 +0200

    add goole file and tag for the webside to be indexed in google

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
 	<link rel="stylesheet" href="/style.css" />
 	<link rel="stylesheet" href="/fonts/font-awesome.css" />
+	<meta name="google-site-verification" content="xSZr7WHsNqCIS8a80LMDMf9_GO7dWn_kCHyMd7Cn2pg" />
 	<title>خدمتك علينا</title>
 </svelte:head>
