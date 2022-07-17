ae86aba684fd90e16e565a6096ffdeeea68d59ce
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-17 19:43:27 +0200

    add vite plugin for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			 registerType: 'autoUpdate',
+			 devOptions: {
+				enabled: true
+			  }
+			})]
 };
 
 export default config;
