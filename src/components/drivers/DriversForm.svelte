6bc2a4c9171d523213d24cd81643a9233a710b31
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-21 00:41:40 +0200

    formate component

--- src/components/drivers/DriversForm.svelte
+++ src/components/drivers/DriversForm.svelte
@@ -3,24 +3,26 @@
 	import AddDriver from './AddDriver.svelte';
 	let isopen = false;
 </script>
+
 <div class="driverform">
 	<button on:click={() => (isopen ? (isopen = false) : (isopen = true))}>
-		{isopen ? "^ إخفاء الاستمارة":"انضم الي برنامج السواقين"}
+		{isopen ? '^ إخفاء الاستمارة' : 'انضم الي برنامج السواقين'}
 	</button>
 	{#if isopen}
 		{#key isopen}
-			<div in:slide={{ x: -50, duration: 250 }} out:slide={{ x: -50, duration: 250 }}>
+			<div in:slide={{ duration: 250 }} out:slide={{ duration: 250 }}>
 				<AddDriver />
 			</div>
 		{/key}
 	{/if}
 </div>
+
 <style>
-	.driverform{
+	.driverform {
 		background-color: rgb(224, 225, 226);
-		padding: .2rem ;
+		padding: 0.2rem;
 	}
-	button{
-		padding: .2rem 30%;
+	button {
+		padding: 0.2rem 30%;
 	}
 </style>
