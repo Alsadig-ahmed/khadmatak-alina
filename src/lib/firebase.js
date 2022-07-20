9ef55272b398553554fb05425cbe89246da9ec98
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-20 10:00:22 +0200

    initialize Firebase for using the SDKs

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDP_v7B-dVDOBaaJh1Dlz4PNO-P4TbV-wA",
+  authDomain: "alina-go.firebaseapp.com",
+  projectId: "alina-go",
+  storageBucket: "alina-go.appspot.com",
+  messagingSenderId: "656424437808",
+  appId: "1:656424437808:web:c1c7b933901162cd4185f5",
+  measurementId: "G-1BJTLFZ1GX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
