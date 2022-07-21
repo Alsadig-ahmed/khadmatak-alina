33c8f1b28a2661980988e3b62f46372a980ccbfb
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-21 23:50:06 +0200

    add footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,12 @@
 <script>
+	import Wrapper from '$lib/Wrapper.svelte';
+	import Footer from '../components/Footer.svelte';
 	import Head from '../components/Head.svelte';
 </script>
 
 <Head />
 <slot />
+
+<Wrapper>
+	<Footer />
+</Wrapper>
