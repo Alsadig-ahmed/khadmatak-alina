22ecd24c84a88703d8418874562ddf68c28f93e3
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-21 23:47:32 +0200

    add footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+</script>
+
+<hr />
+<footer>
+	<div class="">
+		<a href="https:bit.ly/alina-app" target="_blank" rel="noopener noreferrer">
+			<p>تنزل تطبيق الاندرويد</p>
+		</a>
+		<p><a href="/faq"> الاسئلة الشاعة </a></p>
+	</div>
+	<div class="divider" />
+	<div class="">
+		<p><a href="/about">ما هي خدمتك علينا</a></p>
+	<p><a href="/tirihal/register">الانصمام لبرنامج السواقين</a></p>
+
+		<!-- <p>تقديم مقترح او ملاحظة</p> -->
+	</div>
+</footer>
+
+<style lang="scss">
+	footer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+	}
+	.divider {
+		margin: 0 1rem;
+		border-right: 1px solid #eee;
+		height: 4rem;
+	}
+</style>
