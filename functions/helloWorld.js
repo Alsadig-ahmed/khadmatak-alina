b81cd0e3e768abccf8f7cb65635aee40f48d7cac
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-21 16:16:02 +0200

    add firebase to the project

--- functions/helloWorld.js
+++
@@ -1,9 +0,0 @@
-// /.netlify/functions/helloWorld
-exports.handler = async function (event, context) {
-	// process.env.helloWorld = 'helloWorld';
-    console.log(event);
-    return{
-        statusCode:200,
-        body:JSON.stringify({mesg:'hello World'})
-    }
-};
